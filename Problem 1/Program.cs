1d4497ec17744712c80cddff271dabb79d51a4e4
JYE <dexxaye@gmail.com>
2014-08-18 13:22:59 -0400

    problem 1

---
+++ Problem 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MAX_NUMBER = 1000;
+            int[] divisors = { 3, 5 };
+            int sum = 0;
+
+            for (int i = MAX_NUMBER - 1; i > 0; i--)
+            {
+                foreach (int divisor in divisors)
+                {
+                    if (i % divisor == 0)
+                    {
+                        sum += i;
+                        break;
+                    }
+                }
+            }
+
+            Console.Write("Sum of all natural number less than {0} divisible by 3 or 5 is {1}", MAX_NUMBER, sum);
+            Console.Read();
+        }
+    }
+}
